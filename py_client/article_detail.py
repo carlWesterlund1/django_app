7084dc991faaf4c149d6b09a9f026b86ac949a55
Westerlund Carl <carl.westerlund@turku.fi>
2022-04-29 13:12:50 +0300

    added article_detail in py_client

---
+++ py_client/article_detail.py
@@ -0,0 +1,14 @@
+import requests
+
+id = 17 # id of article to change
+endpoint="http://127.0.0.1:8000/api/articles/{0}".format(id) 
+
+get_response = requests.get(endpoint)  
+
+#delete_response = requests.delete(endpoint) 
+
+#put_response = requests.put(endpoint, {'title': ['api-article'], 'body': ['article created with api-client'], 'slug': ['api-article'], 'thumb': ['']})  
+
+
+
+print({'data' : get_response.json()})
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,10 @@
 import requests
 
 
-endpoint="http://127.0.0.1:8000/api/articles/16"
+endpoint="http://127.0.0.1:8000/api/"
 
-#get_response = requests.get(endpoint)
-get_response = requests.delete(endpoint, ) # {'title': ['api-article'], 'body': ['article created with api-client'], 'slug': ['api-article'], 'thumb': ['']}
+get_response = requests.get(endpoint)  
 
-print({"data": get_response.json()})
+#post_response = requests.post(endpoint, {'title': ['api-article'], 'body': ['article created with api-client'], 'slug': ['api-article'], 'thumb': ['']})  
+
+print({'data' : get_response.json()})
