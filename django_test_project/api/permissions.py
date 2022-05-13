1f35a336d74cccf559a249a9eed1d336a0be86e8
Westerlund <carl.westerlund@turku.fi>
2022-05-13 10:22:08 +0300

    added perms_map to IsStaffEditorPermission in api/permissions.py

--- django_test_project/api/permissions.py
+++ django_test_project/api/permissions.py
@@ -2,12 +2,27 @@ from rest_framework import permissions
 
 
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
+    perms_map = {
+        'GET': ['%(app_label)s.view_%(model_name)s'],
+        'OPTIONS': [],
+        'HEAD': [],
+        'POST': ['%(app_label)s.add_%(model_name)s'],
+        'PUT': ['%(app_label)s.change_%(model_name)s'],
+        'PATCH': ['%(app_label)s.change_%(model_name)s'],
+        'DELETE': ['%(app_label)s.delete_%(model_name)s'],
+    }
+
     def has_permission(self, request, view):
+        if not request.user.is_staff:
+            return False
+        return super().has_permission(request, view)
+
+    """def has_permission(self, request, view):
         print('permissions method running')
         user = request.user
         print(user.get_all_permissions())
         if user.is_staff:
-            if user.has_perm("articles.add_article"):
+            if user.has_perm("articles.add_article"): # "app_name.verb_model_name"
                 return True
             if user.has_perm("articles.delete_article"):
                 return True
@@ -16,4 +31,6 @@ class IsStaffEditorPermission(permissions.DjangoModelPermissions):
             if user.has_perm("articles.view_article"):
                 return True
             return False
-        return False 
+        return False """
+
+
