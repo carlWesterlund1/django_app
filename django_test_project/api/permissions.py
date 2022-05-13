551a098e83c7d881e21a320b27fa7036cd214790
Westerlund <carl.westerlund@turku.fi>
2022-05-13 09:55:27 +0300

    new conditions within IsStaffEditorPermission in api/permissions.py

--- django_test_project/api/permissions.py
+++ django_test_project/api/permissions.py
@@ -3,9 +3,17 @@ from rest_framework import permissions
 
 class IsStaffEditorPermission(permissions.DjangoModelPermissions):
     def has_permission(self, request, view):
+        print('permissions method running')
         user = request.user
+        print(user.get_all_permissions())
         if user.is_staff:
-            return True
+            if user.has_perm("articles.add_article"):
+                return True
+            if user.has_perm("articles.delete_article"):
+                return True
+            if user.has_perm("articles.change_article"):
+                return True
+            if user.has_perm("articles.view_article"):
+                return True
+            return False
         return False 
-
-   
